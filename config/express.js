1759c09d7761ea5045f8bae3a6815170f6cc77c6
Gabriel Alves <itsmealves@gmail.com>
2016-06-30 19:09:03 -0300

    Refactor static files

--- config/express.js
+++ config/express.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "public");
+app.set("views", "resources/views");
 app.set("PORT", process.env.PORT || 3000);
 
-//configura middlewares
+app.use(express.static('resources/assets'));
 
 module.exports = app;
