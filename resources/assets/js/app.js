052297896bec355157fa302775fb2f7b5e98bc20
Gabriel Alves <itsmealves@gmail.com>
2016-06-30 19:17:11 -0300

    playing audio captured

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,24 @@
-alert("hue");
+var config = {
+    audio: true,
+    video: false
+};
+
+const audio = new AudioContext();
+
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+
+function handle(stream) {
+    const input = audio.createMediaStreamSource(stream);
+    input.connect(audio.destination);
+}
+
+if(navigator.getUserMedia) {
+    navigator.getUserMedia(config, handle, error => {
+        console.log(error.name);
+    });
+}
+else {
+    console.log("I can't use that! :/")
+}
