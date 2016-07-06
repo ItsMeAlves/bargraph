cf95fd67d9726fcdb2b30503dd41e0731071d008
Gabriel Alves <itsmealves@gmail.com>
2016-07-06 11:37:21 -0300

    working basic frequency bargraph

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,10 @@ var config = {
 };
 
 const audio = new AudioContext();
+const board = document.querySelector("#drawBoard");
+board.setAttribute("height", window.innerHeight);
+board.setAttribute("width", window.innerWidth);
+const boardContext = board.getContext("2d");
 
 navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
@@ -11,6 +15,38 @@ navigator.getUserMedia = navigator.getUserMedia ||
 
 function handle(stream) {
     const input = audio.createMediaStreamSource(stream);
+    const WIDTH = board.offsetWidth;
+    const HEIGHT = board.offsetHeight;
+    var analyser = audio.createAnalyser();
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
+
+    function draw() {
+        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        drawVisual = requestAnimationFrame(draw);
+        analyser.getByteTimeDomainData(dataArray)
+        boardContext.fillStyle = 'rgb(0, 0, 0)';
+        boardContext.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for(var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i]*4;
+
+            boardContext.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+            boardContext.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
+
+            x += barWidth + 1;
+        }
+    };
+
+    draw();
+
+    input.connect(analyser);
     input.connect(audio.destination);
 }
 
